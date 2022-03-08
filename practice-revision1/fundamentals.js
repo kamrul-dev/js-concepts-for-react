8f47343c4932fa0d80f14225c1e840a76325bd21
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-08 14:56:59 +0600

    explore condition

--- practice-revision1/fundamentals.js
+++ practice-revision1/fundamentals.js
@@ -7,3 +7,42 @@ const motherName = 'Hafiza'; // const : redeclare and reassign is not possible
 // when we think that we have to change the value in future that time we will use the let variable
 let myPhone = 'Samsung'; // let: reassign is possible , redeclare is not possible
 myPhone = 'iphone'
+
+
+
+/* 
+    ২.১ কিভাবে কন্ডিশন লিখতে হয়, ছয় রকমের কন্ডিশন (>, <, ===, !==, >=. <=) কোনটা কোন জিনিসের জন্য ইউজ করবে। এছাড়াও && বা ।। দিয়ে কিভাবে একাধিক কন্ডিশন এর মধ্যে দুইটাই ফুলফিল করতে হবে আবার দুইটার যেকোন একটা ফুলফিল করতে হবে, সেটা কিভাবে করবে.
+*/
+const a = 5;
+const b = 7;
+if(a > b || b > a){
+    console.log("a is greater than b")
+}
+else{
+    console.log("a is not grater than b");
+}
+
+
+const p = 1;
+const q = 3;
+if(p < q && q > p){
+    console.log("Both condition is true");
+}
+else{
+    console.log("Both condition is not true");
+}
+
+if(a === b){
+    console.log("a is equal to b");
+}
+else{
+    console.log("a is not equal to b")
+}
+
+
+if(a !== b){
+    console.log("a is equal to b");
+}
+else{
+    console.log("a is not equal to b")
+}
