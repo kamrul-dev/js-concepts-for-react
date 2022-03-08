38a53c8a4dbd2677e2dbf1165837c9b79e4f4fff
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-08 14:11:40 +0600

    explore const and let variable keyword

---
+++ practice-revision1/fundamentals.js
@@ -0,0 +1,9 @@
+/* 
+    ১. কিভাবে let, const দিয়ে ভেরিয়েবল লিখতে হয়,কখন কোনটা ইউজ করে তুমি কি জানো ?
+*/
+// when we think that we do not have to change the value, that time we will use const variable
+const motherName = 'Hafiza'; // const : redeclare and reassign is not possible
+
+// when we think that we have to change the value in future that time we will use the let variable
+let myPhone = 'Samsung'; // let: reassign is possible , redeclare is not possible
+myPhone = 'iphone'
